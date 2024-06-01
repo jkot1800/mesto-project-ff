8fdaa42f7617409815e3e7b350c6fa82064202f7
Евгений Котов <direktekotov@yandex.ru>
2024-06-01 16:52:15 +0300

    Добавил карточки на страницу и реализовал их удаление со страницы

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addNewCard(item) {
+    const cardListItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const deleteButton = cardListItem.querySelector('.card__delete-button');
+    cardListItem.querySelector('.card__image').src = item.link;
+    cardListItem.querySelector('.card__image').alt = item.name;
+    cardListItem.querySelector('.card__title').textContent = item.name;
+   
+    deleteButton.addEventListener('click', closeCard)
 
+    cardList.append(cardListItem);
+    return cardListItem;
+}
 // @todo: Функция удаления карточки
-
+function closeCard(evt) {
+    evt.target.closest('.places__item').remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(item) {
+    addNewCard(item);
+});
+
+
+
+
